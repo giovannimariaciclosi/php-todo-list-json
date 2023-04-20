a63b59f8d1d67c0be907d33b2fcaf4695b152361
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-04-20 15:13:49 +0200

    feat: exercise completed

--- js/script.js
+++ js/script.js
@@ -11,11 +11,42 @@ createApp({
   },
 
   methods: {
-    
+    getTodos() {
+
+      axios.get('./server.php').then(response => {
+        this.todos = response.data;
+      });
+    },
+
+
+    addTodo() {
+
+      let data = {
+        newTodo: '',
+      }
+      
+      // console.log(data);
+
+      data.newTodo = this.newTodo;
+
+      // console.log(data.newTodo.name);
+
+      axios.post('./server.php', data, {headers: {'Content-Type': 'multipart/form-data'}}).then(response => {
+        console.log("La chiamata POST ha avuto successo");
+        // console.log(response);
+
+        this.getTodos();
+      });
+
+      // console.log(this.newTodo);
+      this.newTodo = "";
+    },
+      // console.log(this.newTodo);
   },
 
+
   mounted() {
-    
+    this.getTodos();
   },
 
 }).mount('#app');
--- server.php
+++ server.php
@@ -1,10 +1,44 @@
 <?php
 
+if(isset($_POST['newTodo'])) {
 
-$stringaDelFile = file_get_contents('todos.json');
+  // $newTodo = [
+  //   'name' => $_POST['newTodo'],
+  //   'completed' => false,
+  // ];
 
-$todos = json_decode($stringaDelFile);
+  $todosJSON = file_get_contents('todos.json');
 
-header('Content-type: application/json');
+  $todos = json_decode($todosJSON);
 
-echo json_encode($todos);
+  // var_dump($_POST['newTodo']);
+
+  // $key = array("name", "completed");
+  // $value = array($_POST['newTodo'], false);
+
+  // $newTodo = array_combine($key, $value);
+  // var_dump($newTodo);
+
+  $todos[] = $_POST['newTodo'];
+
+  // var_dump($todos);
+
+  $newTodoJSON = json_encode($todos);
+
+  file_put_contents('todos.json', $newTodoJSON);
+
+
+} else {
+
+  $JSONtoString = file_get_contents('todos.json');
+  
+  $todos = json_decode($JSONtoString);
+
+
+
+  
+  header('Content-type: application/json');
+  
+  echo json_encode($todos);
+
+}
